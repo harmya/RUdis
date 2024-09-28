7b90a28d2f489ddb827bfd1344d579939a5defe5
harmya <hvbhatt@purdue.edu>
2024-09-28 00:53:33 -0400

    set get methods

--- src/main.rs
+++ src/main.rs
@@ -2,9 +2,13 @@ use tokio::io::{AsyncReadExt, AsyncWriteExt};
 use tokio::net::TcpListener;
 use tokio::net::TcpStream;
 
+#[derive(Debug)]
 enum CommandType {
     PING,
     WHAT,
+    ECHO,
+    SET,
+    GET,
     ERROR
 }
 
@@ -13,6 +17,9 @@ impl CommandType {
         match command {
             "PING" => CommandType::PING,
             "WHAT" => CommandType::WHAT,
+            "ECHO" => CommandType::ECHO,
+            "SET" => CommandType::SET,
+            "GET" => CommandType::GET,
             _ => CommandType::ERROR
         }
     }
@@ -32,14 +39,40 @@ fn require_data(data: String) -> String {
     }
 }
 
-fn execute_command(command: CommandType, data: String) -> String {
+fn execute_command(command: CommandType, data: String, storage: &mut std::collections::HashMap<String, String>) -> String {
+    println!("Executing command: {:?} with data: {}", command, data);
     match command {
-        CommandType::PING => {
+        CommandType::ECHO => {
             let pong_message = require_data(data);
-            return format!("PONG {}\n", pong_message);
+            return format!("{}\n", pong_message);
+        },
+        CommandType::PING => {
+            return "PONG\n".to_string();
         },
         CommandType::WHAT => {
             return "This is a implementation of redis using Rust. Checkout my github: www.github.com/harmya\n".to_string();
+        },
+        CommandType::SET => {
+            let mut data_parts = data.splitn(2, ' ');
+            let key = data_parts.next().unwrap_or("");
+            let value = data_parts.next().unwrap_or("");
+            let key = require_data(key.to_string());
+            let value = require_data(value.to_string());
+            let response = format!("Success");
+            storage.insert(key, value).expect("Failed to insert key value pair");
+            return response;
+        },
+        CommandType::GET => {
+            let key = require_data(data);
+            let value = storage.get(&key);
+            match value {
+                Some(value) => {
+                    return format!("Value: {}\n", value);
+                },
+                None => {
+                    return format!("Value: Not found\n");
+                }
+            }
         },
         CommandType::ERROR => {
             return String::from("Error executing command, not found\n");
@@ -47,7 +80,7 @@ fn execute_command(command: CommandType, data: String) -> String {
     }
 }
 
-fn respond(request: String) -> String {
+fn respond(request: String, storage: &mut std::collections::HashMap<String, String>) -> String {
     let request = request.trim();
 
     if request.is_empty() {
@@ -69,11 +102,13 @@ fn respond(request: String) -> String {
     // println!("Request Command: {}", request_command);
     // println!("Request Data: {}", request_data);
 
-    return execute_command(CommandType::from_str(request_command), request_data.to_string());
+    return execute_command(CommandType::from_str(request_command), request_data.to_string(), storage);
 }
 
 async fn handle_client(mut stream: TcpStream) {
     let mut buffer = [0; 1024];
+    let mut storage: std::collections::HashMap<String, String> = std::collections::HashMap::new();
+
     loop {
         let bytes_read = stream.read(&mut buffer).await.expect("Failed to read message in the buffer");
         if bytes_read == 0 {
@@ -82,7 +117,7 @@ async fn handle_client(mut stream: TcpStream) {
         }
         let request = String::from_utf8_lossy(&buffer[..bytes_read]);
         println!("Request:\n{}", request);
-        let response  = respond(request.into_owned());
+        let response  = respond(request.into_owned(), &mut storage);
         stream.write(response.as_bytes()).await.expect("Failed to send response");
     }
 }
